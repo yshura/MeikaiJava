3a9ed9e0aeba7173b15b6cee0b00758e0d3e0e9b
EC2 Default User <[yudai.0404.0404@gmail.com]>
2022-10-31 07:06:43 +0000

    画面に文字を表示

--- chapter01/Hello.java
+++ chapter01/Hello.java
@@ -1,6 +1,6 @@
 class Hello{
   public static void main(String[] args) {
     System.out.println("初めてのJavaプログラム。");
-    System.out.println('画面に出力しています。');
+    System.out.println("画面に出力しています。");
   }
 }
